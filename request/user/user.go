285476aea6fd1879aa6501c8a26deff760876fe9
Sharykhin <siarhei.sharykhin@gmail.com>
2019-09-09 23:17:24 +0300

    fixed request error

--- request/user/user.go
+++ request/user/user.go
@@ -1,12 +1,14 @@
 package user
 
-import "github.com/Sharykhin/go-payments/entity"
+import (
+	"github.com/Sharykhin/go-payments/core/type"
+)
 
 type (
 	RegisterRequest struct {
-		FirstName string            `json:"first_name" binding:"required,max=80"`
-		LastName  entity.NullString `json:"last_name" binding:"max=80"`
-		Email     string            `json:"email" binding:"required,email,max=80"`
-		Password  string            `json:"password" binding:"required,min=8"`
+		FirstName string           `json:"first_name" binding:"required,max=80"`
+		LastName  types.NullString `json:"last_name" binding:"max=80"`
+		Email     string           `json:"email" binding:"required,email,max=80"`
+		Password  string           `json:"password" binding:"required,min=8"`
 	}
 )
